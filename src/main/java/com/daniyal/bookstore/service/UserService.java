6dc1659c3741d341fec32196985abe0cadb4dcfc
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-07-31 21:20:14 +0530

    created interface for UserService

---
+++ src/main/java/com/daniyal/bookstore/service/UserService.java
@@ -0,0 +1,13 @@
+package com.daniyal.bookstore.service;
+
+import com.daniyal.bookstore.entity.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserService {
+    User registerUser(User user);
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+    List<User> getAllUsers();
+}
