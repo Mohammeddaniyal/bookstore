4ade4e22acf7a56a31413539bdac9c8ae89d9087
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-08-01 16:04:00 +0530

    handled exception

--- src/main/java/com/daniyal/bookstore/service/UserServiceImpl.java
+++ src/main/java/com/daniyal/bookstore/service/UserServiceImpl.java
@@ -1,10 +1,13 @@
 package com.daniyal.bookstore.service;
 
 import com.daniyal.bookstore.entity.User;
+import com.daniyal.bookstore.exceptions.UserAlreadyExistsException;
 import com.daniyal.bookstore.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -14,10 +17,27 @@ public class UserServiceImpl implements UserService{
 
     @Autowired
     private UserRepository userRepository;
-
+    @Autowired
+    private PasswordEncoder passwordEncoder;
     @Override
     public User registerUser(User user) {
+
+     List<String> errorMessages=new ArrayList<>();
+        Optional<User> userOptional=userRepository.findByEmail(user.getEmail());
+        if(userOptional.isPresent())
+        {
+            errorMessages.add("Email already exists");
+        }
+        if(userRepository.findByUsername(user.getUsername()).isPresent())
+        {
+            errorMessages.add("User already exists");
+        }
+        if(!errorMessages.isEmpty())
+        {
+            throw new UserAlreadyExistsException(errorMessages);
+        }
         user.setRoles(Arrays.asList("CUSTOMER"));
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 
