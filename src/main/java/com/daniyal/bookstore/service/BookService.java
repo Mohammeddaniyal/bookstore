9c776b60dc0617357330d32ba28ecf042d1f7297
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-08-03 21:53:38 +0530

    Feat BookService

---
+++ src/main/java/com/daniyal/bookstore/service/BookService.java
@@ -0,0 +1,16 @@
+package com.daniyal.bookstore.service;
+
+import com.daniyal.bookstore.dto.BookRequestDTO;
+import com.daniyal.bookstore.dto.BookResponseDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Optional;
+
+public interface BookService {
+    BookResponseDTO createBook(BookRequestDTO bookRequest);
+    Optional<BookResponseDTO> getBookById(Long id);
+    Page<BookResponseDTO> getAllBooks(Pageable pageable);
+    Optional<BookResponseDTO> updateBook(Long id,BookRequestDTO bookRequest);
+    void deleteBook(Long id);
+}
