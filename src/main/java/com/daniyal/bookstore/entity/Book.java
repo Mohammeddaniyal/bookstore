fc5d4a8183449bd42073dd8ad7103f4c3886fc35
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-08-03 13:47:23 +0530

    Feat Book Entity field

--- src/main/java/com/daniyal/bookstore/entity/Book.java
+++ src/main/java/com/daniyal/bookstore/entity/Book.java
@@ -1,5 +1,24 @@
 package com.daniyal.bookstore.entity;
 
+import jakarta.persistence.Entity;
+import lombok.*;
 
+import java.math.BigDecimal;
+
+@Setter
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Book {
+    private Long id;
+
+    private String  title;
+    private String author;
+    private String isbn;
+
+    private String description;
+    private BigDecimal price;
+    private int quantity;
 }
