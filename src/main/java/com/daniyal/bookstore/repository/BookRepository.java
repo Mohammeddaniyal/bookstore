22bb2702840243cc5b1bd4d78f5a129c03ad0cb1
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-08-03 21:52:18 +0530

    created repository

---
+++ src/main/java/com/daniyal/bookstore/repository/BookRepository.java
@@ -0,0 +1,11 @@
+package com.daniyal.bookstore.repository;
+
+import com.daniyal.bookstore.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface BookRepository extends JpaRepository<Book,Long> {
+    Optional<Book> findByIsbn(String isbn);
+    Optional<Book> findByAuthorAndTitle(String author,String title);
+}
