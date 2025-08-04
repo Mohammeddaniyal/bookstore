89227175cecec68734508105d08b9a9f7c14523b
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-08-04 22:28:12 +0530

    resolved a bug

--- src/main/java/com/daniyal/bookstore/exceptions/ImmutableFieldException.java
+++ src/main/java/com/daniyal/bookstore/exceptions/ImmutableFieldException.java
@@ -1,6 +1,7 @@
 package com.daniyal.bookstore.exceptions;
 
 public class ImmutableFieldException extends RuntimeException {
-    public ImmutableFieldException(String s) {
+    public ImmutableFieldException(String message) {
+        super(message);
     }
 }
