8e4f90647c455d37745d5bd188069655ea7c7128
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-08-04 20:36:55 +0530

    Created new exception class

---
+++ src/main/java/com/daniyal/bookstore/exceptions/DataPersistenceException.java
@@ -0,0 +1,7 @@
+package com.daniyal.bookstore.exceptions;
+
+public class DataPersistenceException extends RuntimeException {
+    public DataPersistenceException(String message) {
+        super(message);
+    }
+}
