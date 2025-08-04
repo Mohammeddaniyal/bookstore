f9a358e7e4b8329a91bc88bbcb9051bb4dfa68e1
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-08-04 21:39:55 +0530

    Created dto for partial update

---
+++ src/main/java/com/daniyal/bookstore/dto/BookUpdateDTO.java
@@ -0,0 +1,18 @@
+package com.daniyal.bookstore.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Setter
+@Getter
+@Builder
+public class BookUpdateDTO {
+    private String title;
+    private String author;
+    private String description;
+    private BigDecimal price;
+    private int quantity;
+}
