9bf8457012a0b1322d54d531f7cfe810f346b71f
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-08-04 22:24:17 +0530

    refactored update into two services one is partial and another is full

--- src/main/java/com/daniyal/bookstore/service/BookService.java
+++ src/main/java/com/daniyal/bookstore/service/BookService.java
@@ -12,6 +12,7 @@ public interface BookService {
     BookResponseDTO createBook(BookRequestDTO bookRequest);
     Optional<BookResponseDTO> getBookById(Long id);
     Page<BookResponseDTO> getAllBooks(Pageable pageable);
-    BookResponseDTO updateBook(Long id, BookUpdateDTO bookRequest);
+    BookResponseDTO partialUpdateBook(Long id, BookUpdateDTO bookRequest);
+    BookResponseDTO fullUpdateBook(Long id,BookRequestDTO bookRequest);
     void deleteBook(Long id);
 }
