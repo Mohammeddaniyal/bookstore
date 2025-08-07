378906a70fe3c347f67430a11565b65518f099cf
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-08-07 18:24:00 +0530

    Created Order entity class

---
+++ src/main/java/com/daniyal/bookstore/entity/Order.java
@@ -0,0 +1,28 @@
+package com.daniyal.bookstore.entity;
+
+import com.daniyal.bookstore.enums.OrderStatus;
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne(optional = false)
+    @JoinColumn(name="user_id")
+    private User user;
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+    @OneToMany(mappedBy = "order",cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<OrderItem> items=new ArrayList<>();
+
+    private BigDecimal totalAmount;
+    private LocalDateTime createdAt;
+
+
+}
