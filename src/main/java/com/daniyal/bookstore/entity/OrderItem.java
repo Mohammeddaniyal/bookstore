2280a4a95ca5b6cd23d6908740c930af89ffcacc
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-08-07 18:16:06 +0530

    Annotated the fields

--- src/main/java/com/daniyal/bookstore/entity/OrderItem.java
+++ src/main/java/com/daniyal/bookstore/entity/OrderItem.java
@@ -1,13 +1,19 @@
 package com.daniyal.bookstore.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 
 import java.math.BigDecimal;
 
 @Entity
 public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @ManyToOne(optional=false)
+    @JoinColumn(name="book_id")
     private Book book;
+    @ManyToOne(optional = false)
+    @JoinColumn(name="order_id")
     private Order order;
     private int quantity;
     private BigDecimal subTotal;
