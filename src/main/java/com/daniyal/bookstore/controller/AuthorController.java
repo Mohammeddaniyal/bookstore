9712a4c516a4dbdbbf699f7f1741e16cce8a7f36
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-08-08 10:28:01 +0530

    Created controller for author

---
+++ src/main/java/com/daniyal/bookstore/controller/AuthorController.java
@@ -0,0 +1,31 @@
+package com.daniyal.bookstore.controller;
+
+import com.daniyal.bookstore.dto.AuthorRequestDTO;
+import com.daniyal.bookstore.dto.AuthorResponseDTO;
+import com.daniyal.bookstore.service.AuthorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/author")
+public class AuthorController {
+    @Autowired
+    private AuthorService authorService;
+
+
+    @PostMapping
+    public ResponseEntity<AuthorResponseDTO> createAuthor(@RequestBody AuthorRequestDTO authorRequest)
+    {
+        return new ResponseEntity<>(authorService.createAuthor(authorRequest),HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AuthorResponseDTO>> getAllAuthors()
+    {
+        return new ResponseEntity<>(authorService.getAllAuthors(),HttpStatus.OK);
+    }
+}
