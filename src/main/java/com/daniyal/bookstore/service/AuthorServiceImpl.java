4b4b3e441817afcfbf21144d4d7aa8793c97eb6b
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-08-08 10:27:44 +0530

    Creted service class

---
+++ src/main/java/com/daniyal/bookstore/service/AuthorServiceImpl.java
@@ -0,0 +1,43 @@
+package com.daniyal.bookstore.service;
+
+
+import com.daniyal.bookstore.dto.AuthorRequestDTO;
+import com.daniyal.bookstore.dto.AuthorResponseDTO;
+import com.daniyal.bookstore.entity.Author;
+import com.daniyal.bookstore.exceptions.AuthorAlreadyExistsException;
+import com.daniyal.bookstore.repository.AuthorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class AuthorServiceImpl implements AuthorService {
+
+    @Autowired
+    private AuthorRepository authorRepository;
+
+    @Override
+    public AuthorResponseDTO createAuthor(AuthorRequestDTO request) {
+       if(authorRepository.existsByNameIgnoreCase(request.getName()))
+       {
+           throw new AuthorAlreadyExistsException("Author already exists.");
+       }
+       Author author= Author.builder()
+               .name(request.getName())
+               .build();
+       Author savedAuthor=authorRepository.save(author);
+       return AuthorResponseDTO.builder()
+               .name(savedAuthor.getName())
+               .id(savedAuthor.getId())
+               .build();
+    }
+
+    @Override
+    public List<AuthorResponseDTO> getAllAuthors() {
+        return authorRepository.findAll().stream()
+                .map(author -> AuthorResponseDTO.builder()
+                        .id(author.getId())
+                        .name(author.getName())
+                        .build())
+                .toList();
+    }
+}
