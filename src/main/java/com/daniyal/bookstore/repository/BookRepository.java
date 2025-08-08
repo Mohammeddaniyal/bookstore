84280afea42aba13089ac562c6b3c2338b9fff61
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-08-08 17:01:53 +0530

    Added Custom Query

--- src/main/java/com/daniyal/bookstore/repository/BookRepository.java
+++ src/main/java/com/daniyal/bookstore/repository/BookRepository.java
@@ -2,12 +2,17 @@ package com.daniyal.bookstore.repository;
 
 import com.daniyal.bookstore.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface BookRepository extends JpaRepository<Book,Long> {
     Optional<Book> findByIsbn(String isbn);
-    Optional<Book> findByAuthorAndTitle(String author,String title);
+   // Optional<Book> findByAuthorAndTitle(String author,String title);
     List<Book> findByTitle(String title);
+    @Query("SELECT DISTINCT b from Book b LEFT JOIN FETCH b.authors WHERE b.title=:title")
+    List<Book> findByTitleWithAuthors(@Param("title") String title);
+
 }
