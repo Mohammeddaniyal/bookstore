d1af6d91ef0585a1157150a36e2f0217179b52c6
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-08-08 16:58:49 +0530

    Aadded validation

--- src/main/java/com/daniyal/bookstore/controller/AuthorController.java
+++ src/main/java/com/daniyal/bookstore/controller/AuthorController.java
@@ -3,6 +3,7 @@ package com.daniyal.bookstore.controller;
 import com.daniyal.bookstore.dto.AuthorRequestDTO;
 import com.daniyal.bookstore.dto.AuthorResponseDTO;
 import com.daniyal.bookstore.service.AuthorService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,14 +12,14 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/author")
+@RequestMapping("/api/authors")
 public class AuthorController {
     @Autowired
     private AuthorService authorService;
 
 
     @PostMapping
-    public ResponseEntity<AuthorResponseDTO> createAuthor(@RequestBody AuthorRequestDTO authorRequest)
+    public ResponseEntity<AuthorResponseDTO> createAuthor(@Valid @RequestBody AuthorRequestDTO authorRequest)
     {
         return new ResponseEntity<>(authorService.createAuthor(authorRequest),HttpStatus.CREATED);
     }
