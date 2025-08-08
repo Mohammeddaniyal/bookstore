378ab12a486d2b21ba1445c00e145bccdd5b5b9d
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-08-08 11:36:49 +0530

    Refactored name

--- src/main/java/com/daniyal/bookstore/exceptions/BookNotExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.daniyal.bookstore.exceptions;
-
-public class BookNotExistsException extends RuntimeException {
-    public BookNotExistsException(String message) {
-        super(message);
-    }
-}
---
+++ src/main/java/com/daniyal/bookstore/exceptions/BookNotFoundException.java
@@ -0,0 +1,7 @@
+package com.daniyal.bookstore.exceptions;
+
+public class BookNotFoundException extends RuntimeException {
+    public BookNotFoundException(String message) {
+        super(message);
+    }
+}
