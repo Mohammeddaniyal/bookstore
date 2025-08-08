d251cd40c59f76ac818db9d037e311317d627acf
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-08-08 10:27:20 +0530

    Created DTO for author

---
+++ src/main/java/com/daniyal/bookstore/dto/AuthorRequestDTO.java
@@ -0,0 +1,14 @@
+package com.daniyal.bookstore.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@Builder
+public class AuthorRequestDTO {
+    private String name;
+}
