821ba65573fae220be91025d525bff866cd9fe94
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-08-08 10:27:07 +0530

    Created repository for author

---
+++ src/main/java/com/daniyal/bookstore/repository/AuthorRepository.java
@@ -0,0 +1,8 @@
+package com.daniyal.bookstore.repository;
+
+import com.daniyal.bookstore.entity.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuthorRepository extends JpaRepository<Author,Long> {
+    boolean existsByNameIgnoreCase(String name);
+}
