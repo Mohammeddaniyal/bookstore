00040bbba623047ab0af4b8955006f25b7680baa
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-08-08 11:35:37 +0530

    Declaed new method for getting List of same title books

--- src/main/java/com/daniyal/bookstore/repository/BookRepository.java
+++ src/main/java/com/daniyal/bookstore/repository/BookRepository.java
@@ -3,9 +3,11 @@ package com.daniyal.bookstore.repository;
 import com.daniyal.bookstore.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface BookRepository extends JpaRepository<Book,Long> {
     Optional<Book> findByIsbn(String isbn);
     Optional<Book> findByAuthorAndTitle(String author,String title);
+    List<Book> findByTitle(String title);
 }
