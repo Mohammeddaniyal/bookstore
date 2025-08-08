a4c5ed0cd9968656541da2980c8e5ffdbb1ad7a7
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-08-08 11:51:14 +0530

    Refactored the updateDTO code

--- src/main/java/com/daniyal/bookstore/dto/BookUpdateDTO.java
+++ src/main/java/com/daniyal/bookstore/dto/BookUpdateDTO.java
@@ -5,14 +5,17 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.math.BigDecimal;
+import java.util.Set;
 
 @Setter
 @Getter
 @Builder
 public class BookUpdateDTO {
     private String title;
-    private String author;
+    private Set<Long> authorIds;
+    private String genre;
     private String description;
     private BigDecimal price;
     private Integer quantity;
+    private String imageUrl;
 }
