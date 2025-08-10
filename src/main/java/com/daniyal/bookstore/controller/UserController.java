00aea14ec6087b6dd16f36997ce1958c6ed153bd
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-08-10 21:54:08 +0530

    Addde new s to get all users

--- src/main/java/com/daniyal/bookstore/controller/UserController.java
+++ src/main/java/com/daniyal/bookstore/controller/UserController.java
@@ -14,6 +14,7 @@ import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -72,4 +73,10 @@ public class UserController {
         }
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
+    @GetMapping
+    public ResponseEntity<List<UserResponseDTO>> getAll()
+    {
+        return new ResponseEntity<>(userService.getAllUsers(),HttpStatus.OK);
+    }
+
 }
