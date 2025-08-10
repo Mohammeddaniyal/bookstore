bd2828bbbda632574535cb9e5adfc473147756f5
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-08-10 20:53:49 +0530

    Refactored

--- src/main/java/com/daniyal/bookstore/dto/OrderRequestDTO.java
+++ src/main/java/com/daniyal/bookstore/dto/OrderRequestDTO.java
@@ -1,16 +1,21 @@
 package com.daniyal.bookstore.dto;
 
-import com.daniyal.bookstore.entity.OrderItem;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import lombok.*;
 
 import java.util.List;
 
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class OrderRequestDTO {
 
     @NotNull(message = "Order must contain at least one item")
     @NotEmpty(message = "Order items are required")
     @Valid // <-- this ensures OrderItemDto inside items list are validated
-    private List<OrderItemDTO> items;
+    private List<OrderItemRequestDTO> orderItems;
 }
