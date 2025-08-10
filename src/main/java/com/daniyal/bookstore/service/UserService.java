3281a8cd9dfad3cf6c8d4bd6e66c2ae45ab04708
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-08-10 21:54:15 +0530

    Addde new api to get all users

--- src/main/java/com/daniyal/bookstore/service/UserService.java
+++ src/main/java/com/daniyal/bookstore/service/UserService.java
@@ -1,8 +1,8 @@
 package com.daniyal.bookstore.service;
 
 import com.daniyal.bookstore.dto.LoginRequestDTO;
+import com.daniyal.bookstore.dto.UserResponseDTO;
 import com.daniyal.bookstore.entity.User;
-import jakarta.validation.Valid;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,7 +11,7 @@ public interface UserService {
     User registerUser(User user);
     Optional<User> findByUsername(String username);
     Optional<User> findByEmail(String email);
-    List<User> getAllUsers();
+    List<UserResponseDTO> getAllUsers();
 
     String login(LoginRequestDTO loginRequest);
 }
