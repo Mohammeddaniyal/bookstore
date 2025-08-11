53310c41ed4fc7e8bef67c10efe2d94f13477123
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-08-11 11:19:20 +0530

    Declared new repository method with optimized query for fetching

--- src/main/java/com/daniyal/bookstore/repository/OrderRepository.java
+++ src/main/java/com/daniyal/bookstore/repository/OrderRepository.java
@@ -2,9 +2,17 @@ package com.daniyal.bookstore.repository;
 
 import com.daniyal.bookstore.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order,Long> {
-public List<Order> findAllByUser_Username(String username);
+    public List<Order> findAllByUser_Username(String username);
+    @Query("SELECT o FROM Order o" +
+            "JOIN FETCH o.orderItems oi" +
+            "JOIN FETCH oi.book WHERE o.id=:orderId")
+    Optional<Order> findByIdWithItemsAndBooks(@Param("orderId")Long Id);
+
 }
