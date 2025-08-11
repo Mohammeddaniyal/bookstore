759f5ad4fef602f39daaa21347f3a8fdd5819457
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-08-11 15:46:25 +0530

    FIXED, used prefix ROLE_

--- src/main/java/com/daniyal/bookstore/controller/OrderController.java
+++ src/main/java/com/daniyal/bookstore/controller/OrderController.java
@@ -99,9 +99,9 @@ public class OrderController {
     }
 
     @PatchMapping("/{orderId}/status")
-    @PreAuthorize("hasAuthority('ADMIN')")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     // ADMIN-only: update status of any order
-    public ResponseEntity<Void> updateOrderStatus(@PathVariable Long orderId, @RequestParam OrderStatus orderStatus) {
+    public ResponseEntity<Void> updateOrderStatus(@PathVariable Long orderId, @RequestParam("status") OrderStatus orderStatus) {
     /*
      ADMIN-only: Updates the status of an order (e.g., PENDING -> SHIPPED).
  - Access restricted via @PreAuthorize("hasAuthority('ADMIN')").
