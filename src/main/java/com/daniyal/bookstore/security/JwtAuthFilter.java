9840fee465e65ef1f374885174802db7851e7132
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-08-11 18:49:57 +0530

    Removed unwanted imports

--- src/main/java/com/daniyal/bookstore/security/JwtAuthFilter.java
+++ src/main/java/com/daniyal/bookstore/security/JwtAuthFilter.java
@@ -11,20 +11,18 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
-import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 
 import java.io.IOException;
 
 
-public class JwtAuthFilter extends OncePerRequestFilter {
+public class JwtAuthFilter extends OncePerRequestFilter{
 
 
     private final HandlerExceptionResolver handlerExceptionResolver;
