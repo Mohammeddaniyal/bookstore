a13dbd49047efff3d91e495cee39f5f8cedd35ad
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-08-15 22:14:27 +0530

    pr

--- src/main/java/com/daniyal/bookstore/service/OrderService.java
+++ src/main/java/com/daniyal/bookstore/service/OrderService.java
@@ -3,6 +3,9 @@ package com.daniyal.bookstore.service;
 import com.daniyal.bookstore.dto.OrderRequestDTO;
 import com.daniyal.bookstore.dto.OrderResponseDTO;
 import com.daniyal.bookstore.enums.OrderStatus;
+import jakarta.transaction.Transactional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
@@ -11,6 +14,13 @@ public interface OrderService {
     OrderResponseDTO getOrderById(Long orderId, String email, boolean isAdmin);
     List<OrderResponseDTO> listOrdersForUser(String targetEmail,String loggedInEmail,boolean isAdmin);
     List<OrderResponseDTO> listAllOrders();
+
+    Page<OrderResponseDTO> listAllOrders(Pageable pageable);
+
+
+    Page<OrderResponseDTO> filterOrders(
+            OrderStatus status, String email, Pageable pageable);
+
     void cancelOrder(Long orderId, String email, boolean isAdmin);
     void updateOrderStatus(Long orderId, OrderStatus status);
 }
