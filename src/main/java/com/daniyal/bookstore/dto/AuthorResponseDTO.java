1f8bdd45191e377e8249e25c0f3c83d01e1ff9d3
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-08-17 18:37:58 +0530

    Add comprehensive Swagger annotations to all DTOs for improved API documentation

--- src/main/java/com/daniyal/bookstore/dto/AuthorResponseDTO.java
+++ src/main/java/com/daniyal/bookstore/dto/AuthorResponseDTO.java
@@ -1,5 +1,6 @@
 package com.daniyal.bookstore.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -9,7 +10,10 @@ import lombok.Setter;
 @Getter
 @AllArgsConstructor
 @Builder
+@Schema(description = "Response DTO representing an author")
 public class AuthorResponseDTO {
+    @Schema(description = "Unique identifier of the author", example = "1")
     private Long id;
+    @Schema(description = "Author's full name", example = "Robert C. Martin")
     private String name;
 }
