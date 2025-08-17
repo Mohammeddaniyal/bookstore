8d784e44ed6b9b84b354f7a92ec0bdb1f7617b80
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-08-17 21:43:38 +0530

    Add comprehensive Swagger annotations to all DTOs for improved API documentation

--- src/main/java/com/daniyal/bookstore/dto/PaymentResponseDTO.java
+++ src/main/java/com/daniyal/bookstore/dto/PaymentResponseDTO.java
@@ -1,5 +1,6 @@
 package com.daniyal.bookstore.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 @Getter
@@ -7,8 +8,14 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Schema(description = "Response after payment processing")
 public class PaymentResponseDTO {
+    @Schema(description = "Indicates whether payment succeeded", example = "true")
     private boolean success;
+
+    @Schema(description = "Message describing the payment result", example = "Mock payment successful")
     private String message;
+
+    @Schema(description = "ID of the order related to payment", example = "1001")
     private Long orderId;
 }
