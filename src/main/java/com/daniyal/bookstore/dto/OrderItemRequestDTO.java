f150ae888d18a22983b33cb7c6a12c6a9e1ad9d5
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-08-17 17:49:25 +0530

    Added Validations parts

--- src/main/java/com/daniyal/bookstore/dto/OrderItemRequestDTO.java
+++ src/main/java/com/daniyal/bookstore/dto/OrderItemRequestDTO.java
@@ -1,6 +1,7 @@
 package com.daniyal.bookstore.dto;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.*;
 
 import java.math.BigDecimal;
@@ -13,6 +14,7 @@ import java.math.BigDecimal;
 public class OrderItemRequestDTO {
 
         @NotNull(message = "Book ID cannot be null")
+        @Positive(message = "Book ID cannot be negative or zero")
         private Long bookId;
 
         @Min(value = 1, message = "Quantity must be at least 1")
